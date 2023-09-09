e9b38c42d0613bd13b4fb192591826ae68230277
YHOSEF MAYORGA - C <mayorgalive@hotmail.com>
2023-09-09 11:43:25 -0500

    clase cliente ampliado

--- config/clientes/admin.py
+++ config/clientes/admin.py
@@ -3,8 +3,10 @@ from .models import *
 
 # Register your models here.
 
-@admin.register(Cliente)
-class MikrotikAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'cedula')
-    search_fields = ('nombre', 'cedula')
-    list_filter = ('nombre', 'cedula')
+# @admin.register(Cliente)
+# class MikrotikAdmin(admin.ModelAdmin):
+#     list_display = ('nombre', 'cedula')
+#     search_fields = ('nombre', 'cedula')
+#     list_filter = ('nombre', 'cedula')
+
+admin.site.register(Cliente)
--- config/clientes/models.py
+++ config/clientes/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+from .choices import *
 
 # Create your models here.
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
-    cedula = models.CharField(max_length=50)
+    tipodocu = models.CharField(max_length=3, choices=tipodocu, default='CC')
+    documento = models.CharField(max_length=50, blank=True)
+    pais = models.CharField(max_length=50, blank=True)
+    departamento = models.CharField(max_length=50, blank=True)
+    municipio = models.CharField(max_length=50, blank=True)
+    codigopostal = models.CharField(max_length=50, blank=True)
+    direccion = models.CharField(max_length=50, blank=True)
+    corregimiento = models.CharField(max_length=50, blank=True)
+    barrio = models.CharField(max_length=50, blank=True)
+    correoelectronico= models.EmailField(blank=True)
+    telefoo = models.CharField(max_length=50, blank=True)
+    celular = models.CharField(max_length=50, blank=True)
 
-    def __str__(self):
-        return f'{self.nombre} - {self.cedula}'
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
